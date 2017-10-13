ca6abafe884332352a6115ab65810226348e148f
TheDonten <32191011+TheDonten@users.noreply.github.com>
2017-10-13 11:08:43 +0300

    Create Первое задание.cpp

---
+++ Первое задание.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+#include <sstream>
+
+using namespace std;
+int main ()
+{
+	int i, j=0;
+	char a[80];
+	for(i=0; i<80; i++){
+		cin.get(a[i]);
+		j++;
+		if(a[i]=='\n'){
+			break;
+		}
+	}
+	for(i=0; i<=j; i++){
+		if(a[i]==' '){
+			cout<<"%20";
+		}
+		else{
+			cout<<a[i];
+		}
+	}
+return 0;
+}
