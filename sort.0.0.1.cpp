8ccf1540d459782472af04c9b9389bbf72a6476e
TheDonten <32191011+TheDonten@users.noreply.github.com>
2017-10-27 11:52:43 +0300

    Create sort.0.0.1.cpp

---
+++ sort.0.0.1.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <utility>
+#include <sstream>
+using namespace std;
+ 
+bool read(double *array,  int size)
+{
+	bool k=true;
+	string mas;
+	getline(cin,mas);
+	istringstream stream(mas);
+	for(int i = 0; i<size; i++)
+	{
+		if(!(stream>>array[i]))
+		{k = false;
+		return false;}
+	}
+   return true;
+ } 
+ void selection_sort (double* array,  int size) {
+	
+	for (unsigned int i=0; i<size; i++) {
+		
+		for (unsigned j=i+1; j<size; j++) {
+			
+			if (array[i] > array[j]) {
+			
+				swap (array[i], array[j]);
+			}
+		}
+	}
+}
+ void print_array(double * array, unsigned int size)
+{
+	for (unsigned int i = 0; i < size; i++)
+	{
+		cout << array[i] << " ";
+	}
+}
+ int main()
+ {
+bool k;
+int size;
+cin>>size;
+cin.get();
+double *array = new double[size];
+if(read(array, size))
+	{ 
+		selection_sort(array, size);
+		print_array(array, size);
+		cin.get();
+	}
+else
+	{
+		cout << "An error has occured while reading numbers from line";
+		cin.get();
+	}
+	delete array;
+    return 0;
+
+ 	
+ }
