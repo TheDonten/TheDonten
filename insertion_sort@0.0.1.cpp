a940fa29bef957287ea0bf4a2f6f537de8c83192
TheDonten <32191011+TheDonten@users.noreply.github.com>
2017-11-03 09:47:24 +0300

    Create insertion_sort@0.0.1.cpp

---
+++ insertion_sort@0.0.1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <utility>
+#include <sstream>
+using namespace std;
+ 
+bool read(double *array, unsigned int size)
+{
+	bool k=true;
+	string mas;
+	getline(cin,mas);
+	istringstream stream(mas);
+	for(int i = 0; i<size; i++)
+	{
+		if(!(stream>>array[i]))
+		{k = false;
+		return false;}
+	}
+   return true;
+ } 
+ void selection_sort (double* array, unsigned int size) 
+ {
+   for (unsigned int i=1; i<size; i++) 
+	{
+	  for (unsigned j=i; j>0 && array[j-1]>array[j];j--) 
+		{
+		  swap(array[j-1],array[j]); 
+		}
+	}
+}
+ void print_array(double * array, unsigned int size)
+{
+	for (unsigned int i = 0; i < size; i++)
+	{
+		cout << array[i] << " ";
+	}
+}
+ int main()
+ {
+
+unsigned int size;
+cin>>size;
+cin.get();
+double *array = new double[size];
+if(read(array, size))
+	{ 
+		selection_sort(array, size);
+		print_array(array, size);
+		cin.get();
+	}
+     else
+	{
+		cout << "An error has occured while reading numbers from line";
+		cin.get();
+	}
+	delete array;
+        return 0;
+
+ 	
+ }
