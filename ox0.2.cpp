406974f62e6d518b83724f3491d118785e239da4
TheDonten <32191011+TheDonten@users.noreply.github.com>
2017-11-14 15:37:49 +0300

    Update ox0.2.cpp

--- ox0.2.cpp
+++ ox0.2.cpp
@@ -48,52 +48,41 @@ void show_move(char array[5][5], char op) {
   cout << ++k << ". quit" << endl;
 }
 bool win_game(char array[5][5]) {
-  bool check = false;
-  for (unsigned int i = 0; i < 5 && !check; i++) {
-    for (unsigned int j = 0; j < 5; j++) {
-      if (j == 0) {
-        if ((array[i][j] == 'X' || array[i][j] == 'O') &&
-            array[i][j] == array[i][j + 1] && array[i][j] == array[i][j + 2] &&
-            array[i][j] == array[i][j + 3] && array[i][j] == array[i][j + 4]) {
-          check = true;
-          return true;
-          break;
-        }
-      }
-      if (i == 0) {
-        if ((array[i][j] == 'X' || array[i][j] == 'O') &&
-            array[i][j] == array[i + 1][j] && array[i][j] == array[i + 2][j] &&
-            array[i][j] == array[i + 3][j] && array[i][j] == array[i + 4][j]) {
-          check = true;
-          return true;
-          break;
-        }
-      }
-
-      if (i == 0 && j == 0) {
-        if ((array[i][j] == 'X' || array[i][j] == 'O') &&
-            array[i][j] == array[i + 1][j + 1] &&
-            array[i][j] == array[i + 2][j + 2] &&
-            array[i][j] == array[i + 3][j + 3] &&
-            array[i][j] == array[i + 4][j + 4]) {
-          check = true;
-          return true;
-          break;
-        }
-      }
-      if (i == 0 && j == 4) {
-        if ((array[i][j] == 'X' || array[i][j] == 'O') &&
-            array[i][j] == array[i + 1][j - 1] &&
-            array[i][j] == array[i + 2][j - 2] &&
-            array[i][j] == array[i + 3][j - 3] &&
-            array[i][j] == array[i + 4][j - 4]) {
-          check = true;
-          return true;
-          break;
-        }
+  for (unsigned int i = 0; i < 5; i++) {
+    int j = 0;
+    if (array[i][j] == 'X' || array[i][j] == 'O') {
+      if (array[i][j] == array[i][j + 1] && array[i][j] == array[i][j + 2] &&
+          array[i][j] == array[i][j + 3] && array[i][j] == array[i][j + 4]) {
+        return true;
+        break;
       }
     }
   }
+  for (unsigned int j = 0; j < 5; j++) {
+    int i = 0;
+    if (array[i][j] == 'X' || array[i][j] == 'O') {
+      if (array[i][j] == array[i + 1][j] && array[i][j] == array[i + 2][j] &&
+          array[i][j] == array[i + 3][j] && array[i][j] == array[i + 4][j]) {
+        return true;
+        break;
+      }
+    }
+  }
+
+  if (array[0][0] == 'X' || array[0][0] == 'O') {
+    if (array[0][0] == array[1][1] && array[0][0] == array[2][2] &&
+        array[0][0] == array[3][3] && array[0][0] == array[4][4]) {
+      return true;
+    }
+  }
+  if (array[0][4] == 'X' || array[0][4] == 'O') {
+    if (array[0][4] == array[1][3] && array[0][4] == array[2][2] &&
+        array[0][0] == array[3][1] && array[0][0] == array[4][0]) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 bool proverka(char array[5][5]) {
