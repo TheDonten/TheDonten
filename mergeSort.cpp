71720f89aaf4dd1d3228286cf7a36b328368cf51
TheDonten <32191011+TheDonten@users.noreply.github.com>
2017-11-20 18:17:53 +0300

    Update mergeSort.cpp

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -17,27 +17,30 @@ bool read(double *array, unsigned int size) {
   return true;
 }
 void mergeSort(double *array, int l, int r) {
-  if(l==r) return;//границы сомкнулись
-  int mid=(l+r)/2;
-  mergeSort(array,l,mid);
-  mergeSort(array,mid+1,r);
-  int i=l;
-  int j=mid+1;
-  int *tmp=new int[r-l+1];//доп. массив
-  for(int step=0;step<r-l+1;step++){//для всех элементов доп.массива
-  	if((j>r) || ((i<=mid) && (array[i]<array[j]))){ // j>r для того чтобы последний элемент записался доп.массива 
-  		tmp[step]=array[i];
-  		i++;
-	  }
-    else{
-    	tmp[step]=array[j];
-    	j++;
-	}
+  if (l == r) return;  //границы сомкнулись
+  int mid = (l + r) / 2;
+  mergeSort(array, l, mid);
+  mergeSort(array, mid + 1, r);
+  int i = l;
+  int j = mid + 1;
+  int *tmp = new int[r - l + 1];  //доп. массив
+  for (int step = 0; step < r - l + 1;
+       step++) {  //для всех элементов доп.массива
+    if ((j > r) || ((i <= mid) &&
+                    (array[i] < array[j]))) {  // j>r для того чтобы последний
+                                               // элемент записался доп.массива
+      tmp[step] = array[i];
+      i++;
+    } else {
+      tmp[step] = array[j];
+      j++;
+    }
   }
-  for (int step = 0; step < r - l + 1; step++)//копируем в исходный массив
+  for (int step = 0; step < r - l + 1; step++) {  //копируем в исходный массив
     array[l + step] = tmp[step];
-}
-delete[] tmp;
+  }
+
+  delete[] tmp;
 }
 void print_array(double *array, unsigned int size) {
   for (unsigned int i = 0; i < size; i++) {
@@ -45,25 +48,23 @@ void print_array(double *array, unsigned int size) {
   }
 }
 int main() {
-   int size;
+  int size;
   string string;
   getline(cin, string);
   istringstream stream(string);
-  if (!(stream >> size && (size>0))) {
+  if (!(stream >> size && (size > 0))) {
     cout << "An error has occured while reading numbers from line";
     return -1;
   }
   double *array = new double[size];
   if (read(array, size)) {
-    mergeSort(array, 0,size-1);
+    mergeSort(array, 0, size - 1);
     print_array(array, size);
-    
+
   }
- delete[] array;
+
   else {
     cout << "An error has occured while reading numbers from line";
-   
   }
-  
-  
+  delete[] array;
 }
